59e32a7903a366046d07d7f27ce6b8a56ee457ff
Sharik <sharikkarimi08@gmail.com>
2026-01-29 06:00:14 -0800

    new commit

---
+++ src/exceptionExample/example2.java
@@ -0,0 +1,24 @@
+package exceptionExample;
+
+public class example2 {
+
+    public static void main(String[] args){
+
+        int [] arr = {10,20,30,40,50};
+        try{
+
+            System.out.println("Try block started");
+            System.out.println(arr[5]);
+
+        }catch(ArrayIndexOutOfBoundsException e){
+
+            System.out.println("Catch block started");
+            System.out.println(e.getMessage());
+            System.out.println("catch block ended");
+        }catch(Exception e){
+
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Program Execution flow is working fine");
+    }
+}
